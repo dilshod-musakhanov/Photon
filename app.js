db8fbea47f209e8bee12d537ff7a098f36922bf1
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-01-07 14:10:50 +0400

    refactoring search function

--- app.js
+++ app.js
@@ -15,16 +15,22 @@ function updateInput(e) {
     searchValue = e.target.value;
 }
 
-async function curatedPhotos() {
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15", 
-    {
-        methods: "GET",
-        headers: {
-            Accept: "aplication/json",
-            Authorization: auth
-        }
-    });
+
+async function fetchApi(url) {
+    const dataFetch = await fetch(url,
+        {
+            methods: "GET",
+            headers: {
+                Accept: "aplication/json",
+                Authorization: auth
+            }
+        } 
+    );
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data){
     data.photos.forEach(photo => {
         const galleryImg = document.createElement("div");
         galleryImg.classList.add("gallery-img");
@@ -32,29 +38,17 @@ async function curatedPhotos() {
         <p>${photo.photographer}</p>
         `;
         gallery.appendChild(galleryImg);
-    })
+    });
+}
+
+async function curatedPhotos() {
+    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15"); 
+    generatePictures(data);
 }
 
 async function searchPhotos(query) {
-    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=1`, 
-    {
-        methods: "GET",
-        headers: {
-            Accept: "aplication/json",
-            Authorization: auth
-        }
-    });
-    const data = await dataFetch.json();
-    data.photos.forEach(photo => {
-        const galleryImg = document.createElement("div");
-        galleryImg.classList.add("gallery-img");
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>
-        `;
-        gallery.appendChild(galleryImg);
-    })
-
-    
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=1`);
+    generatePictures(data);    
 }
 
 curatedPhotos();
