774481b3cb1065d6920cd733953b0f7b0bd1f48c
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-01-07 13:47:19 +0400

    set search function

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const auth = "563492ad6f917000010000016d4b1e8d5f674d8ea3cc9b0ea07a42c0";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
+
+//event listeners 
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    searchPhotos(searchValue);
+})
+
+function updateInput(e) {
+    searchValue = e.target.value;
+}
 
 async function curatedPhotos() {
     const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15", 
@@ -22,6 +33,28 @@ async function curatedPhotos() {
         `;
         gallery.appendChild(galleryImg);
     })
+}
+
+async function searchPhotos(query) {
+    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=1`, 
+    {
+        methods: "GET",
+        headers: {
+            Accept: "aplication/json",
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        <p>${photo.photographer}</p>
+        `;
+        gallery.appendChild(galleryImg);
+    })
+
+    
 }
 
 curatedPhotos();
