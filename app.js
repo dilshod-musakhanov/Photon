dd4727767c2310d64b0ca36dc77faf6afce101d6
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-01-08 00:35:39 +0400

    adjust search

--- app.js
+++ app.js
@@ -34,12 +34,23 @@ function generatePictures(data){
     data.photos.forEach(photo => {
         const galleryImg = document.createElement("div");
         galleryImg.classList.add("gallery-img");
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>
-        `;
+        galleryImg.innerHTML = `
+            <div class ="gallery-info">
+            <p>${photo.photographer}</p>
+            <a href=${photo.src.original} target="_blank">Download</a>
+            </div>;
+            <img src=${photo.src.large}></img>
+            `;
         gallery.appendChild(galleryImg);
     });
 }
+
+function clear() {
+    gallery.innerHTML = "";
+    searchInput.value = "";
+}
+
+
 
 async function curatedPhotos() {
     const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15"); 
@@ -47,7 +58,8 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=1`);
+    clear();
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15`);
     generatePictures(data);    
 }
 
