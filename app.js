4b5401963a8617eae1e8f6ee6b45258a4028d0ed
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-01-07 13:00:44 +0400

    fetch images from API

---
+++ app.js
@@ -0,0 +1,27 @@
+const auth = "563492ad6f917000010000016d4b1e8d5f674d8ea3cc9b0ea07a42c0";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+async function curatedPhotos() {
+    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15", 
+    {
+        methods: "GET",
+        headers: {
+            Accept: "aplication/json",
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        <p>${photo.photographer}</p>
+        `;
+        gallery.appendChild(galleryImg);
+    })
+}
+
+curatedPhotos();
